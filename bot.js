b3b198d4f6deae5f539a9bffc0d60ac701eed3b3
Glitch (mercury-spy) <none>
2017-10-19 23:58:44 +0000

    Checkpoint

--- bot.js
+++ bot.js
@@ -5,7 +5,12 @@ let Discord = require("discord.js");
 let client = new Discord.Client();
 
 client.on("message", function (message) {
-  console.log(message);
+  console.log(message.content);
+  
+  if (message.content === "hello wheybot") {
+    message.channel.send(hello <@>!);
+  }
+  
 });
 
 client.login(process.env.SECRET);
