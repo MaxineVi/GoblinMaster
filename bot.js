0cd2498f2f308a8460fb2f2e8135b14bd5dd3a23
Glitch (mercury-spy) <none>
2017-10-20 00:31:42 +0000

    Checkpoint

--- bot.js
+++ bot.js
@@ -14,24 +14,34 @@ client.on("message", function (message) {
     // Remove the prefix
     message.content = message.content.replace(commandPrefix, "");
     
+    // Find firstword
     let firstWord = message.content.split(" ")[0];
+    
+    if (message.content.includes(" ")) {
+      
+      message.content = message.content.substring(firstWord.length, message.content.length)
+      
+    }
   
+    // Hello command
     if (message.content === "hello wheybot") {
       // <@userid> is the syntax Discord uses for a mention
       message.channel.send("hello <@" + message.author.id +">!");
     }
 
+    // Echo command
     if (firstWord === "echo") {
 
       //send message with only the part of the message from the end of the "firstword" to the end of the whole message
-      message.channel.send(message.content.substring(firstWord.length, message.content.length));
-      
-      //"echo hello"
-      //.substring(from,to)
+      message.channel.send();
 
     }
-
-
+    
+    if (firstWord === "d") {
+      
+      
+        
+    }
 
     console.log(firstWord);
     
