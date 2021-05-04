f1b25a6a5aa6a7f153c8f1e42f103fc88415a18a
Glitch (wheybot) <none>
2021-05-04 07:06:22 +0000

    🐾📹 Checkpoint ./bot.js:516539/148:31526457/16088

--- bot.js
+++ bot.js
@@ -10,226 +10,205 @@ let fs = require("fs");
 
 let commandPrefix = "!";
 
-client.on("message", function (message) {
+client.on("message", function(message) {
   // console.log(message.content);
-  
+
   if (message.content.startsWith(commandPrefix)) {
-    
     // Remove the prefix
     message.content = message.content.replace(commandPrefix, "");
-    
+
     // Find firstword
     let firstWord = message.content.split(" ")[0];
-    
+
     // Get everything after firstword
     let afterFirstWord = "";
-    
+
     if (message.content.includes(" ")) {
-      
-      afterFirstWord = message.content.substring(firstWord.length + 1, message.content.length);
-      
+      afterFirstWord = message.content.substring(
+        firstWord.length + 1,
+        message.content.length
+      );
     }
-  
+
     // Hello command
     if (message.content === "hello wheybot") {
       // <@userid> is the syntax Discord uses for a mention
-      message.channel.send("hello <@" + message.author.id +">!");
+      message.channel.send("hello <@" + message.author.id + ">!");
     }
 
     // Echo command
     if (firstWord === "echo") {
-
       //send message with only the part of the message from the end of the "firstword" to the end of the whole message
       message.channel.send(afterFirstWord);
+    }
 
-    }
-    
     if (firstWord === "d") {
-      
       diceRollCommand(afterFirstWord, message);
-        
     }
-    
-    if (/^d\d+/.exec(message.content)){
+
+    if (/^d\d+/.exec(message.content)) {
       diceRollCommand(/d(.*)$/.exec(message.content)[1], message);
     }
-    
+
     if (firstWord === "c") {
-      
       calcCommand(afterFirstWord, message);
-      
     }
-    
+
     if (firstWord === "dw") {
-      
       printTextCommandDW(afterFirstWord, message);
-      
     }
-    
+
     if (firstWord === "hw") {
-      
       printTextCommandHW(afterFirstWord, message);
-      
     }
-    
+
     if (firstWord === "roll") {
-      
       diceRollCommand2d6(afterFirstWord, message);
-      
     }
-    
-    if (/^roll[\+\-]?\d+/.exec(message.content)){
+
+    if (/^roll[\+\-]?\d+/.exec(message.content)) {
       diceRollCommand2d6(/^roll(.*)$/.exec(message.content)[1], message);
     }
   }
-  
 });
 
-let printTextCommandDW = function (argument, message) {
-  
+let printTextCommandDW = function(argument, message) {
   // Remove all non-alphanumeric characters
   // argument = argument.replace(/\W/g, '');
-  
-  
-  // readFile takes the following arguments: path to file, callback function  
-  fs.readFile("./texts/dwrules.txt", "utf8", function (error, data) {
+
+  // readFile takes the following arguments: path to file, callback function
+  fs.readFile("./texts/dwrules.txt", "utf8", function(error, data) {
     if (error) {
       message.channel.send("text file could not be accessed");
     } else {
-      
       let fileSection;
-      
+
       let controlSequence = "---";
-      
+
       // Split string on ---argument and get everything after it
       fileSection = data.split(controlSequence + argument)[1];
-      
+
       if (!fileSection) {
         message.channel.send("Couldn't find that rule.");
         return false;
       }
-            
+
       // Now we need to remove everything after the following --- (or else it's end of file)
       fileSection = fileSection.split(controlSequence)[0];
-            
-      
+
       let embed = new Discord.MessageEmbed({
-        "title": argument,
-        "description": fileSection
+        title: argument,
+        description: fileSection
       });
-      
+
       message.channel.send(embed);
     }
   });
-  
-//   // readFile takes the following arguments: path to file, callback function  
-//   fs.readFile("./texts/" + argument + ".txt", "utf8", function (error, data) {
-//     if (error) {
-//       message.channel.send("couldn't find that text");
-//     } else {
-//       console.log(data)
-      
-//       message.channel.send(data);
-//     }
-//   });
-  
+
+  //   // readFile takes the following arguments: path to file, callback function
+  //   fs.readFile("./texts/" + argument + ".txt", "utf8", function (error, data) {
+  //     if (error) {
+  //       message.channel.send("couldn't find that text");
+  //     } else {
+  //       console.log(data)
+
+  //       message.channel.send(data);
+  //     }
+  //   });
 };
 
-let printTextCommandHW = function (argument, message) {
-    
-  fs.readFile("./texts/hwrules.txt", "utf8", function (error, data) {
+let printTextCommandHW = function(argument, message) {
+  fs.readFile("./texts/hwrules.txt", "utf8", function(error, data) {
     if (error) {
       message.channel.send("text file could not be accessed");
     } else {
-      
       let fileSection;
-      
+
       let controlSequence = "---";
-      
+
       fileSection = data.split(controlSequence + argument)[1];
-      
+
       if (!fileSection) {
         message.channel.send("Couldn't find that rule.");
         return false;
       }
-            
+
       fileSection = fileSection.split(controlSequence)[0];
-            
-      
+
       let embed = new Discord.MessageEmbed({
-        "title": argument,
-        "description": fileSection
+        title: argument,
+        description: fileSection
       });
-      
+
       message.channel.send(embed);
     }
   });
 };
 
-let calcCommand = function (expression, message) {
-  
+let calcCommand = function(expression, message) {
   try {
-  
     let result = mexp.eval(expression);
-  
+
     message.channel.send(result);
-    
   } catch (e) {
-    
-    message.channel.send("Couldn't calculate that. Expression parser says: *" + e.message + "*");
-    
+    message.channel.send(
+      "Couldn't calculate that. Expression parser says: *" + e.message + "*"
+    );
   }
-  
-}
+};
 
-let diceRollCommand = function (diceInput, message) {
+let diceRollCommand = function(diceInput, message) {
   
-  let splitDiceInput = diceInput.split("*");
+  if (message.content.includes("*")) {
+    
+    let splitDiceInput = diceInput.split("*");
 
-  let numSides = splitDiceInput[0];
-  let numRolls = splitDiceInput[1];
-  
-  console.log(numSides)
-  console.log(numRolls)
-  
-  if (numRolls) {
-    // if numRolls isn't a number, OR if it's a number less than 1
-    if (isNaN(numRolls) || numRolls < 1) {
-      message.channel.send("The number of rolls is not valid.");
-      return false; // exit the function without doing anything else
+    let numSides = splitDiceInput[0];
+    let numRolls = splitDiceInput[1];
+
+    console.log(numSides);
+    console.log(numRolls);
+
+    if (numRolls) {
+      // if numRolls isn't a number, OR if it's a number less than 1
+      if (isNaN(numRolls) || numRolls < 1) {
+        message.channel.send("The number of rolls is not valid.");
+        return false; // exit the function without doing anything else
+      }
+    } else {
+      numRolls = 1;
     }
-  } else {
-    numRolls = 1;
-  }
 
-  //if NOT not a number (=== is a number) AND if it exists AND if it's greater than 1
-  if (numSides && !isNaN(numSides) && numSides > 1) {
+    //if NOT not a number (=== is a number) AND if it exists AND if it's greater than 1
+    if (numSides && !isNaN(numSides) && numSides > 1) {
+      let dieTotal = 0;
 
-    let dieTotal = 0;
-    
-    let dieResults = [];
-    
-    // for "i" from zero to the number of rolls we want, run this loop i times
-    for (let i = 0; i < numRolls; i++) {
-      
-      let thisRoll = getRandomInt(1, numSides);
-      
-      dieResults.push(thisRoll);
-      
-      dieTotal = dieTotal + thisRoll;
-      
+      let dieResults = [];
+
+      // for "i" from zero to the number of rolls we want, run this loop i times
+      for (let i = 0; i < numRolls; i++) {
+        let thisRoll = getRandomInt(1, numSides);
+
+        dieResults.push(thisRoll);
+
+        dieTotal = dieTotal + thisRoll;
+      }
+
+      message.channel.send(
+        "Roll results: " + dieResults.toString() + "\nTotal: " + dieTotal
+      );
+    } else {
+      message.channel.send(
+        "The number of sides must be provided as a number greater than 1."
+      );
     }
-    
-    message.channel.send("Roll results: " + dieResults.toString() + "\nTotal: " + dieTotal);
-
-  } else {
-    message.channel.send("The number of sides must be provided as a number greater than 1.");
   }
-  
-}
+  else if (message.content.includes("+")){
+    let 
+  }
+};
 
-let diceRollCommand2d6 = function (numInput, message) {
-  
+let diceRollCommand2d6 = function(numInput, message) {
   // numInput is raw user input
   // numToAdd is the parsed number
   let numToAdd = 0;
@@ -244,32 +223,32 @@ let diceRollCommand2d6 = function (numInput, message) {
   } else {
     numToAdd = parseInt(numInput);
   }
-  
+
   let dieTotal = 0;
   let dieResults = [];
-  
+
   // set first die roll
-  dieResults.push( getRandomInt(1,6) ); // first roll
-  dieResults.push( getRandomInt(1,6) ); // second roll
+  dieResults.push(getRandomInt(1, 6)); // first roll
+  dieResults.push(getRandomInt(1, 6)); // second roll
   // set total
   dieTotal = dieResults[0] + dieResults[1];
-  
+
   let resultTotal = dieTotal + numToAdd;
-  
-  let rollMessage = "Roll results: " + dieResults.toString() + "\nTotal:\t" + dieTotal;
+
+  let rollMessage =
+    "Roll results: " + dieResults.toString() + "\nTotal:\t" + dieTotal;
   if (numToAdd != 0) {
     rollMessage += " + " + numToAdd + " = " + resultTotal;
   }
-  
+
   message.channel.send(rollMessage);
-  
-}
+};
 
 // Random integer function
-let getRandomInt = function (min, max) {
+let getRandomInt = function(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
-}
+};
 
 client.login(process.env.SECRET);
